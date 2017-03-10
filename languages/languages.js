27718125d3b3244a252b6275ee8deda01d2a54ab
Maas Lalani <maas@live.ca>
2017-03-09 22:38:42 -0500

    Extracted languages object into its own file

---
+++ languages/languages.js
@@ -0,0 +1,15 @@
+const languages = {
+  python: {
+    open_multiline_comment: '\"\"\"',
+    close_multiline_comment: '\"\"\"',
+    extension: 'py',
+    command: 'py'
+  },
+  java: {
+    open_multiline_comment: '/*',
+    close_multiline_comment: '*/',
+    extension: 'java'
+  }
+}
+
+module.exports = languages
