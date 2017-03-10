56a0b61cb7eda2685428b9322141683cf294e004
Maas Lalani <maas@live.ca>
2017-03-09 22:36:14 -0500

    Added solution checking abilities

--- README.md
+++ README.md
@@ -20,36 +20,51 @@ Note the API works with all 583 problems on the site, however some questions may
 
 ## CLI :construction:
 
+### Installation
+
 To use the command-line interface simply install the npm module globally
 
 ```bash
 $ npm install -g euley
 ```
 
-and navigate to your project euler solutions directory
+and navigate to your project euler solutions directory or wherever you would like to generate problem files.
 
 ```bash
 $ cd ~/project-euler
 ```
 
-and run `euley <problem>` for example let's do the last problem.
+
+### Generate problem files
+run `euley generate <problem> <language>` for example let's do the first problem.
 
 ```bash
-$ euley 583 && subl pe583.py
+$ euley generate 1 python
+$ subl pe1.py
 ```
 
 ![Command line](https://camo.githubusercontent.com/9942679eac8cc9441aa91f766b68dc05c731315c/687474703a2f2f696d6167652e70726e747363722e636f6d2f696d6167652f32303566363738306239633634663431386633663632653339336136353135392e706e67)
 
-![Text editor](https://camo.githubusercontent.com/a6ae12b77ac075a7591d54c07323f604427006fb/687474703a2f2f696d6167652e70726e747363722e636f6d2f696d6167652f64366130396265393238366334353363626135333866656134366464303333322e706e67)
-
 This will create and open the file in your text editor so you can begin coding.
 
+
+### Checking solutions
+After you've finished coding your solution make sure you print only the solution (should be 1 number).
+
+Now you can check your solution by running `euley solution <problem> <language>`
+
+```bash
+$ euley solution 1 python
+
+Checking "pe1.py" against solution, your output: [spoiler]
+
+Your solution is correct!
+```
+
+
+
 ### TODO
-Add more language support, currently CLI supports only python :snake:
-
-Be able to check solutions :100:
-
-Create multiple files at once for later offline use :airplane:
+Add more language support, currently CLI supports only python
 
 ### Contribute
 Additions to the module are always welcome! :raised_hands:
--- cli.js
+++ cli.js
@@ -2,55 +2,84 @@
 
 // Dependencies
 const filesystem = require('fs')
+const spawn = require('child_process').spawn
 
-const languages = {
-  python: {
-    open_multiline_comment: '\"\"\"',
-    close_multiline_comment: '\"\"\"',
-    extension: 'py'
-  },
-  java: {
-    open_multiline_comment: '/*',
-    close_multiline_comment: '*/',
-    extension: 'java'
-  }
+const languages = require('./languages/languages')
+
+
+
+// Information from command line arguments
+let command = process.argv[2]
+let question = process.argv[3]
+let language = process.argv[4]
+
+
+
+// Generate the file problem 
+if (command == 'g' || command == 'generate') {
+
+  let problems = []
+
+  filesystem.readFile(`${__dirname}/data/project_euler.text`, 'utf-8', (error, data) => {
+
+    if (error) console.log(error)
+    
+    problems = data.match(/Problem(.|\n)*?Answer:\s.+/gm)
+
+    // Create a new file at peX.py
+    language = languages[language]
+    let problem = problems[question]
+
+    let stream = filesystem.createWriteStream(`pe${question}.${language.extension}`)
+
+    // Write out problem content to file
+    stream.once('open', () => {
+
+      stream.write(language.open_multiline_comment)
+
+      stream.write('\n')
+      stream.write(problem)
+      stream.write('\n\n')
+      
+      stream.write(language.close_multiline_comment)
+      stream.end()
+
+    })
+
+    console.log(`\n${problem}\n`)
+    console.log(`File created at pe${question}.${language.extension}`)
+
+  })
 }
 
-let question = process.argv[2]
-let language = process.argv[3]
-let problems = []
 
+// Check the solution for the problem
+else if (command == 's' || command == 'solution') {
 
-filesystem.readFile(__dirname + '/data/project_euler.text', 'utf-8', (error, data) => {
+  let solution
+  let answer
 
-  error ? console.error(error) :
+  // Find the solution in the solutions.text file
+  filesystem.readFile(`${__dirname}/data/solutions.text`, 'utf-8', (error, data) => {
   
-  problems = data.match(/Problem(.|\n)*?Answer:\s.+/gm)
-  createFile(question, problems[question - 1], languages[language])
-
-})
-
-
-function createFile(question, problem, language) {
-
-  // Create a new file at peX.py
-  let stream = filesystem.createWriteStream(`pe${question}.${language.extension}`)
-
-  // Write out problem content to file
-  stream.once('open', () => {
-
-    stream.write(language.open_multiline_comment)
-
-    stream.write('\n')
-    stream.write(problem)
-    stream.write('\n\n')
-    
-    stream.write(language.close_multiline_comment)
-    stream.end()
+    if (error) console.log(error)
+  
+    let solutions = data.split('\n')
+    solution = solutions[question-1]
+    solution = solution.replace(/\d+\.\s/, '')
+    solution = parseInt(solution)
 
   })
 
-  console.log(`\n${problem}\n`)
-  console.log(`File created at pe${question}.${language.extension}`)
+  // Find their answer by executing their solution and retrieving input
+  let file = `pe${question}.py`
+  let process = spawn('py', [file])
+
+  process.stdout.on('data', chunk => {
+    answer = chunk.toString('utf8')
+
+    console.log(`Checking \"${file}\" against solution, your output: ${answer}`)
+    console.log(`Your solution is ${answer == solution ? 'correct.' : 'wrong'}`)
+  })
 
 }
