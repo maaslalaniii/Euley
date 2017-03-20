aba0f6f4a1149d10858030fe34c0227b548c5851
Maas Lalani <maas@live.ca>
2017-03-20 19:27:35 -0400

    Spacing fix

--- commands/help.js
+++ commands/help.js
@@ -1,10 +1,10 @@
 function help() {
 
   console.log('\nPossible commands:')
-  console.log('    generate <problem> <language> - generates a file with the problem\'s description commented out')
-  console.log('    solution <problem> <language> - checks the answer of your program to the problem\'s solution')
-  console.log('    cheat    <problem>            - displays the answer for the specified problem')
-  console.log('    preview  <problem>            - displays the problem on the console ')
+  console.log('  generate <problem> <language> - generates a file with the problem\'s description commented out')
+  console.log('  solution <problem> <language> - checks the answer of your program to the problem\'s solution')
+  console.log('  cheat    <problem>            - displays the answer for the specified problem')
+  console.log('  preview  <problem>            - displays the problem on the console ')
 
 }
 
